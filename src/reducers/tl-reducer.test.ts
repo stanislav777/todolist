91830a89a30c8601dd34ceb66204a78d6aed1292
stanislav777 <solo222@bk.ru>
2021-02-23 21:50:44 +0300

    lesson 8 finish

--- src/reducers/tl-reducer.test.ts
+++ src/reducers/tl-reducer.test.ts
@@ -1,5 +1,5 @@
-import {v1} from 'uuid';
-import {FilterValuesType, TodoListType} from "../App";
+import {v1} from "uuid";
+import {TodoListType} from "../App";
 import {toDoListReducer} from "./tl-reducer";
 
 test('correct todolist should be removed', () => {
@@ -38,28 +38,3 @@ test('correct todolist should be added', () => {
 
 
 
-
-test('correct filter of todolist should be changed', () => {
-    let todolistId1 = v1();
-    let todolistId2 = v1();
-
-    let newFilter: FilterValuesType = "completed";
-
-    const startState: Array<TodoListType> = [
-        {id: todolistId1, title: "What to learn", filter: "all"},
-        {id: todolistId2, title: "What to buy", filter: "all"}
-    ]
-
-
-
-    const action: ActionType = {
-        type: "CHANGE-FILTER",
-        id: todolistId2,
-        filter: newFilter
-    };
-
-    const endState = toDoListReducer(startState, action);
-
-    expect(endState[0].filter).toBe("all");
-    expect(endState[1].filter).toBe(newFilter);
-});
